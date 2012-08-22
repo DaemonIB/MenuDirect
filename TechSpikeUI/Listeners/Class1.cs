ea1c19fb8a386679f242f0596abf25f43f1e99bd
Nathan Nickerson <nickernato@gmail.com>
2012-08-22 14:00:28 -0600

    TechnicalSpike GUI/ BO
    Need DB logicin GUI/BO

---
+++ TechSpikeUI/Listeners/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Listeners
+{
+    public class Class1
+    {
+        public static void EnterItemToDB(String input)
+        {
+            Console.WriteLine("EnterITemToDB input :   " + input);
+
+            //DB Stuff here
+        }
+    }
+}
---
+++ TechSpikeUI/TechSpikeUI/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TechSpikeUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddItem_Click(object sender, RoutedEventArgs e)
+        {
+            // CoolButton Clicked! Let's show our InputBox.
+            InputBox.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputBox.Visibility = System.Windows.Visibility.Collapsed;
+            
+            ItemAdded itemAdded = new ItemAdded();
+
+            itemAdded.inputToDB += Listeners.Class1.EnterItemToDB;
+
+            itemAdded.CallEvent(InputTextBox.Text);
+        }
+    }
+}
